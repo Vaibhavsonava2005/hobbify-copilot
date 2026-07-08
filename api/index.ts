af40ce8d231d6cc2ca754d974d533657f6e31e0f
Vaibhav Sonava <vaibhavsonava2005@gmail.com>
2026-07-08 12:21:16 +0530

    Fix Vercel export signature

--- api/index.ts
+++ api/index.ts
@@ -160,4 +160,11 @@ app.all('/*', (c) => {
   }, 404);
 });
 
-export default handle(app);
+const handler = handle(app);
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
+export const PATCH = handler;
+export const DELETE = handler;
+export const OPTIONS = handler;
+
